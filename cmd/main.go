5499dd1d94a1a5ac9efc6c8de412f1ac2023ff4e
mariownyou <smj510black@gmail.com>
2023-07-04 00:50:44 +0200

    added video support

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/mariownyou/go-twitter-uploader/twitter_uploader"
+import (
+	"os"
+
+	"github.com/mariownyou/go-twitter-uploader/twitter_uploader"
+)
 
 const (
 	consumerKey       = "key"
@@ -11,6 +15,12 @@ const (
 
 func main() {
 	uploader := twitter_uploader.New(consumerKey, consumerSecret, accessToken, accessTokenSecret)
-	file := []byte("file")
-	uploader.Upload("text", file, "filename")
+
+	file, err := os.ReadFile("video.mp4")
+	if err != nil {
+		panic(err)
+	}
+
+	uploader.Upload("text", file, "video.mp4")
+	// uploader.UploadVideo("text", file, "video.mp4")
 }
--- twitter_uploader/uplaoder.go
+++ twitter_uploader/uplaoder.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
+	"net/url"
 	"path/filepath"
 
 	"github.com/dghubble/oauth1"
@@ -51,11 +52,12 @@ func (u *Uploader) Upload(text string, file []byte, filename string) {
 	path := "https://api.twitter.com/2/tweets"
 
 	t := types[filepath.Ext(filename)]
-	if t != "tweet_image" {
-		return
+	var mediaID string
+	if t == "tweet_image" {
+		mediaID = u.uploadImage(file, filename)
+	} else {
+		mediaID = u.uploadVideo(file, filename)
 	}
-
-	mediaID := u.uploadImage(file, filename)
 
 	tweet := Tweet{
 		Text:  text,
@@ -104,4 +106,131 @@ func (u *Uploader) uploadImage(file []byte, filename string) string {
 	return mediaResponse.MediaID
 }
 
-func (u *Uploader) uploadVideo(filename string) {}
+func createBatches(l int) []int {
+	batchSize := 5 * 1024 * 1024
+	times := l / batchSize
+	reminder := l % batchSize
+	batches := make([]int, times)
+	for i := 0; i < times; i++ {
+		batches[i] = batchSize
+	}
+	if reminder > 0 {
+		batches = append(batches, reminder)
+	}
+	return batches
+}
+
+func (u *Uploader) uploadVideo(file []byte, filename string) string {
+	mediaID := u.initUpload(len(file))
+	fmt.Println(mediaID)
+
+	for i, batch := range createBatches(len(file)) {
+		u.appendUpload(file[:batch], filename, i, mediaID)
+	}
+
+	u.finalizeUpload(mediaID)
+	return mediaID
+}
+
+func (u *Uploader) initUpload(size int) string {
+	path := fmt.Sprintf("https://upload.twitter.com/1.1/media/upload.json?command=INIT&total_bytes=%v&media_type=video/mp4", size)
+
+	// create multipart form
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	form := url.Values{}
+	form.Add("command", "INIT")
+	form.Add("total_bytes", fmt.Sprintf("%v", size))
+	form.Add("media_type", "video/mp4")
+
+	// create url variable
+	url, _ := url.Parse(path)
+
+	// set query params
+	url.RawQuery = form.Encode()
+
+	req, _ := http.NewRequest("POST", url.String(), body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	// send request
+	resp, _ := u.Client.Do(req)
+	defer resp.Body.Close()
+	// read response
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("Raw Response Body:\n%v\n", string(respBody))
+
+	type InitResponse struct {
+		MediaID string `json:"media_id_string"`
+	}
+
+	var initResponse InitResponse
+	// unmarshal response
+	json.Unmarshal(respBody, &initResponse)
+	fmt.Printf("Media ID: %v\n", initResponse.MediaID)
+
+	return initResponse.MediaID
+}
+
+func (u *Uploader) appendUpload(file []byte, filename string, segmentIndex int, mediaID string) {
+	path := "https://upload.twitter.com/1.1/media/upload.json"
+	// multipart/form-data
+
+	// create multipart form
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	form := url.Values{}
+	form.Add("command", "APPEND")
+	form.Add("media_id", mediaID)
+	form.Add("segment_index", fmt.Sprintf("%v", segmentIndex))
+
+	url, _ := url.Parse(path)
+	url.RawQuery = form.Encode()
+
+	// add image to multipart form
+	part, _ := writer.CreateFormFile("media", filename)
+	reader := bytes.NewReader(file)
+	io.Copy(part, reader)
+	writer.Close()
+	// build request
+	req, _ := http.NewRequest("POST", url.String(), body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	// send request
+	resp, _ := u.Client.Do(req)
+	defer resp.Body.Close()
+	// read response
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("Raw Response Body:\n%v\n", string(respBody))
+}
+
+func (u *Uploader) finalizeUpload(mediaID string) {
+	path := "https://upload.twitter.com/1.1/media/upload.json?command=FINALIZE"
+
+	// create multipart form
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	form := url.Values{}
+	form.Add("command", "FINALIZE")
+	form.Add("media_id", mediaID)
+
+	// create url variable
+	url, _ := url.Parse(path)
+
+	// set query params
+	url.RawQuery = form.Encode()
+
+	req, _ := http.NewRequest("POST", url.String(), body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	// send request
+	resp, _ := u.Client.Do(req)
+	defer resp.Body.Close()
+	// read response
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("Raw Response Body:\n%v\n", string(respBody))
+
+	resp, err := u.Client.Post(path, "application/json", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
